48f167d4a7b8f0d12de5eb2ff714077237933350
Erik Borela <erikborella@gmail.com>
2021-08-02 21:29:47 -0300

    Implementado verificação das opções do menu

--- CalcTabajara.ConsoleApp/Program.cs
+++ CalcTabajara.ConsoleApp/Program.cs
@@ -6,33 +6,48 @@ namespace CalcTabajara.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Primeiro número:");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Segundo número:");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Digite 1 para soma: ");
-            Console.WriteLine("Digite 2 para subtração: ");
-            double operacao = Convert.ToDouble(Console.ReadLine());
-
-            double resultado;
-
-            switch (operacao)
+            while (true)
             {
-                case 1:
-                    resultado = num1 + num2;
-                    break;
+                Console.WriteLine("Primeiro número:");
+                double num1 = Convert.ToDouble(Console.ReadLine());
 
-                case 2:
-                    resultado = num1 - num2;
-                    break;
 
-                case 3:
-                    resultado = num1 / num2;
-                    break;
+                Console.WriteLine("Segundo número:");
+                double num2 = Convert.ToDouble(Console.ReadLine());
 
-                case 4:
-                    resultado = num1 * num2;
-                    break;
+
+                Console.WriteLine("Digite 1 para soma: ");
+                Console.WriteLine("Digite 2 para subtração: ");
+                Console.WriteLine("Digite 5 para sair: ");
+                double operacao = Convert.ToDouble(Console.ReadLine());
+
+                if (operacao != 1 || operacao != 2 || operacao != 3 || operacao != 4 || operacao != 5)
+                {
+                    Console.WriteLine("Você digitou uma opção invalida, tente novamente");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                double resultado;
+
+                switch (operacao)
+                {
+                    case 1:
+                        resultado = num1 + num2;
+                        break;
+
+                    case 2:
+                        resultado = num1 - num2;
+                        break;
+
+                    case 3:
+                        resultado = num1 / num2;
+                        break;
+
+                    case 4:
+                        resultado = num1 * num2;
+                        break;
+                }
             }
         }
     }
