cb4514592c7d4e5448e311c622edbfb1cec2af40
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-02 21:15:34 -0300

    Add project files.

---
+++ CalcTabajara.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalcTabajara.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro número:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Segundo número:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite 1 para soma: ");
+            Console.WriteLine("Digite 2 para subtração: ");
+            double operacao = Convert.ToDouble(Console.ReadLine());
+
+            switch (operacao)
+            {
+            }
+        }
+    }
+}
