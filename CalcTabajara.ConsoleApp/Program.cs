17bedffc5aa8579d59e930303e8a3d2f00f8abbe
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-02 21:19:51 -0300

    Operacao Subtracao

--- CalcTabajara.ConsoleApp/Program.cs
+++ CalcTabajara.ConsoleApp/Program.cs
@@ -21,6 +21,10 @@ namespace CalcTabajara.ConsoleApp
                 case 1:
                     resultado = num1 + num2;
                     break;
+
+                case 2:
+                    resultado = num1 - num2;
+                    break;
             }
         }
     }
